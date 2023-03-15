53eda6c4ed84b6866406a56340c0e70ce8289adc
christina <andrea.christina.unger@gmail.com>
2023-03-15 08:29:02 +0100

    try: nested array

--- src/google-sheets.js
+++ src/google-sheets.js
@@ -20,11 +20,13 @@ async function appendFisheriesData(items) {
 
   const response = await sheets.spreadsheets.values.append({
     spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
-    range: 'Fisheries',
+    range: 'Fisheries!A:W',
     valueInputOption: 'RAW',
     insertDataOption: 'INSERT_ROWS',
     resource: {
-      values: items.map(item => asFisheriesRow(item))
+      values: [
+        items.map(item => asFisheriesRow(item))
+      ]
     }
   });
 
